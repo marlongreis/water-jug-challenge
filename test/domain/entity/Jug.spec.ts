85915d85d52372217aa4107324c794e1718d818a
Marlon Reis <marlon-reis91@hotmail.com>
2022-08-17 18:22:10 -0300

    test: ensures that euclidean distance return correct value

--- test/domain/entity/Jug.spec.ts
+++ test/domain/entity/Jug.spec.ts
@@ -1,20 +1,16 @@
 import { Jug } from "@/domain"
 
 describe('Jug', () => {
-    let jug: Jug
+    let jug: Jug = undefined
 
     beforeEach(async () => {
-        jug = new Jug(2, 10, 4)
+        jug = new Jug()
     })
 
-    test('should return data with bucket x is fill', () => {
-        const response = jug.pourTheGallonOfWater(2, 0)
-        expect(response).toMatchObject({
-            canTransfer: false,
-            bucketX: 'Fill',
-            bucketY: 'Empty',
-            message: 'Fill bucket x'
-        })
+
+    test('should return 2 when reteceive 2 and 10', () => {
+        const response = Object.getPrototypeOf(jug).euclideanDistance(2, 10)
+        expect(response).toBe(2)
     })
 
 })
