8a504582cc0b4ab2f9db49cc7f26c25756ba75a9
Marlon Reis <marlon-reis91@hotmail.com>
2022-08-17 19:24:48 -0300

    feat: create a method to calculates the most efficient way

--- src/domain/entity/Jug.ts
+++ src/domain/entity/Jug.ts
@@ -1,4 +1,67 @@
+import { JugData } from '@/domain/data/JugData'
+
 export class Jug {
+    constructor (
+        private readonly jugX: number,
+        private readonly jugY: number,
+        private readonly amountWanted: number
+    ) { }
+
+    public calculatesTheMostEfficientWay (): JugData {
+        let bucketX = this.jugX
+        let bucketY = this.jugY
+        const amount = this.amountWanted
+
+        if (bucketX > bucketY) {
+            const t = bucketX
+            bucketX = bucketY
+            bucketY = t
+        }
+
+        if (amount > bucketY) {
+            return new JugData(false, undefined, 'No Solution')
+        }
+
+        if (this.hasNoSolution(amount, bucketX, bucketY)) {
+            return new JugData(false, undefined, 'No Solution')
+        }
+
+        const solution1 = this.pour(bucketY, bucketX, amount)
+        const solution2 = this.pour(bucketX, bucketY, amount)
+
+        const min = Math.min(solution1, solution2)
+
+        return new JugData(true, min, `Most efficient way is with ${min} steps`)
+    }
+
+    private pour (fromCap: number, toCap: number, d: number): number {
+        let from = fromCap
+        let to = 0
+        let step = 1
+
+        while (from !== d && to !== d) {
+            const temp = Math.min(from, toCap - to)
+
+            to += temp
+            from -= temp
+
+            step++
+
+            if (from === d || to === d) { break }
+
+            if (from === 0) {
+                from = fromCap
+                step++
+            }
+
+            if (to === toCap) {
+                to = 0
+                step++
+            }
+        }
+        return step
+    }
+
     private euclideanDistance (x: number, y: number): number {
         if (y === 0) {
             return x
