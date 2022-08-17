34a2f4fd1c7592e3ed30bf0f7874002382e7a85c
Marlon Reis <marlon-reis91@hotmail.com>
2022-08-17 18:40:36 -0300

    feat: create method to verify if problem has a solution

--- src/domain/entity/Jug.ts
+++ src/domain/entity/Jug.ts
@@ -6,4 +6,11 @@ export class Jug {
 
         return this.euclideanDistance(y, x % y)
     }
+
+    private hasNoSolution (bucketX: number, bucketY: number, amount: number): boolean {
+        if ((amount % this.euclideanDistance(bucketY, bucketX)) !== 0) {
+            return true
+        }
+        return false
+    }
 }
