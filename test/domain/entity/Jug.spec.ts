fe551c8d2226bcd1ba5dec025dd5487b8da44a72
Marlon Reis <marlon-reis91@hotmail.com>
2022-08-17 19:26:05 -0300

    test: ensures that method calculates the most efficient way return correct data

--- test/domain/entity/Jug.spec.ts
+++ test/domain/entity/Jug.spec.ts
@@ -4,7 +4,7 @@ describe('Jug', () => {
     let jug: Jug = undefined
 
     beforeEach(async () => {
-        jug = new Jug()
+        jug = new Jug(2, 10, 4)
     })
 
 
@@ -29,5 +29,46 @@ describe('Jug', () => {
         expect(response).toBeFalsy()
     })
 
+    test('should return the smallest number of steps when it is possible to solve', () => {
+        const response = jug.calculatesTheMostEfficientWay()
+
+        expect(response).toMatchObject({
+            solved: true,
+            moreEfficient: 4,
+            message: 'Most efficient way is with 4 steps'
+        })
+    })
+
+
+    test('should return the smallest number of steps when it is possible to solve', () => {
+        const response = new Jug(2, 10, 4).calculatesTheMostEfficientWay()
+
+        expect(response).toMatchObject({
+            solved: true,
+            moreEfficient: 4,
+            message: 'Most efficient way is with 4 steps'
+        })
+    })
+
+    test("should return message 'No Solution' when the amount wanted is greater than jug y", () => {
+        const response = new Jug(2, 10, 40).calculatesTheMostEfficientWay()
+
+        expect(response).toMatchObject({
+            solved: false,
+            moreEfficient: undefined,
+            message: 'No Solution'
+        })
+    })
+
+
+    test("should return message 'No Solution' when not find solution", () => {
+        const response = new Jug(2, 5, 4).calculatesTheMostEfficientWay()
+
+        expect(response).toMatchObject({
+            solved: false,
+            moreEfficient: undefined,
+            message: 'No Solution'
+        })
+    })
 
 })
